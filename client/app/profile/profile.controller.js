906291cede31b78720ce315961785a0d04328a04
johanzap <jfzapata318@misena.edu.co>
2017-11-09 13:06:19 -0500

    Se agrega el modulo de perfil y la actualizacion de los datos en el perfil

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -3,7 +3,32 @@
 (function(){
 
 class ProfileComponent {
-  constructor() {
+  constructor(usersService, $state,authService) {
+    this.usersService = usersService;
+    this.$state = $state;
+    this.authService = authService;
+  }
+
+  $onInit(){
+    this.usersService.get({idUsers:this.authService.getIdUser()}).$promise
+    .then(response=>{
+      this.user = response;
+      console.log('Perfil OK',response);
+    })
+    .catch(err => {
+      console.log('Error Al Mostrar Perfil', err);
+    })
+
+
+    /*this.usersService.query().$promise
+    .then(response=> {
+      this.user = response;
+      console.log('Perfil OK',response);
+        this.$state.go("profile");
+    })
+    .catch(err => {
+      console.log('Error Al Mostrar Perfil', err);
+    })*/
 
   }
 }
@@ -12,7 +37,7 @@ angular.module('socialSoccerApp')
   .component('profile', {
     templateUrl: 'app/profile/profile.html',
     controller: ProfileComponent,
-    controllerAs: 'profileCtrl'
+    controllerAs: 'vm'
   });
 
 })();
